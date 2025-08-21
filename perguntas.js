f05731a1a1c1c530f82907baafbb8d03912ae29c
kawanescola <kawan.miguel.marques@escola.pr.gov.br>
2025-08-21 08:42:27 -0300

    img

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+'matemática',
+'o que é numero primo?',
+'número natural maior que 1 que possui apenas dois divisoresdistintos: 1 e ele mesmo'
+)
+
+criaCartao(
+'Geografia',
+'Qual a capital da Paraná?',
+'A capital do Paraná é Curitiba'
+)
+
+criaCartao(
+'Portuguễs',
+'O que é um verbo?',
+'é uma palavra que expressa uma ação, estado ou fenômeno da natureza,situando-a no tempo.'
+)
+criaCartao(
+'Lingua inglesa',
+'Como se diz olá em inglês?',
+'Olá em ingles é (hello)'
+)
+
